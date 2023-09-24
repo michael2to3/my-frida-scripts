b60ef951ebf281c688c4c7641e481000665d7f6a
Mikhael Gaile <michael2ga3@gmail.com>
2023-09-24 08:48:01 +0300

    chore: format code

--- src/hook_with_arg.js
+++ src/hook_with_arg.js
@@ -1,32 +1,28 @@
 setTimeout(() => {
-  Java.perform(function () {
+  Java.perform(() => {
     Java.enumerateLoadedClasses({
-      onMatch: function (className) {
+      onMatch(className) {
         if (
-          className.includes("ru.bspb") ||
-          className.includes("j30.") ||
-          className.includes("d1.") ||
-          className.includes("fz.") ||
-          className.includes("a0.") ||
-          className.includes("tw.")
+          className.includes("com.example") // <-- change package
         ) {
           try {
-            let classInstance = Java.use(className);
+            const classInstance = Java.use(className);
 
-            let constructorOverloads = classInstance.$init.overloads;
+            const constructorOverloads = classInstance.$init.overloads;
             for (let i = 0; i < constructorOverloads.length; i++) {
               hookMethod(classInstance, "$init", i);
             }
 
-            let methods = classInstance.class.getDeclaredMethods();
-            methods.forEach(function (method) {
-              let methodName = method.getName();
-              let methodStr = method.toString();
+            const methods = classInstance.class.getDeclaredMethods();
+            methods.forEach((method) => {
+              const methodName = method.getName();
+              const methodStr = method.toString();
               if (
                 methodStr.includes("java.lang.String") ||
                 methodStr.includes("java.lang.Number")
               ) {
-                let overloadCount = classInstance[methodName].overloads.length;
+                const overloadCount =
+                  classInstance[methodName].overloads.length;
                 for (let i = 0; i < overloadCount; i++) {
                   try {
                     hookMethod(classInstance, methodName, i);
@@ -46,42 +42,43 @@ setTimeout(() => {
           }
         }
       },
-      onComplete: function () {
+      onComplete() {
         console.log("Class enumeration complete");
       },
     });
   });
 }, 5000);
 
-  function hookMethod(classInstance, methodName, overloadIndex) {
-      classInstance[methodName].overloads[overloadIndex].implementation =
-        function () {
-          for (let i = 0; i < arguments.length; i++) {
-            checkFieldsRecursive(arguments[i]);
-          }
+function hookMethod(classInstance, methodName, overloadIndex) {
+  classInstance[methodName].overloads[overloadIndex].implementation =
+    function () {
+      for (let i = 0; i < arguments.length; i++) {
+        checkFieldsRecursive(arguments[i]);
+      }
 
-          let retval = this[methodName].apply(this, arguments);
+      const retval = this[methodName].apply(this, arguments);
 
-          if (String(retval).includes("1478")) {
-            console.log(
-              "Match found in return value! Class:",
-              classInstance.class.getName(),
-              ", Method:",
-              methodName,
-              ", Overload Index:",
-              overloadIndex,
-            );
-            console.log("Return value:", retval);
-          }
+      if (String(retval).includes("1478")) { // <- replace here
+        console.log(
+          "Match found in return value! Class:",
+          classInstance.class.getName(),
+          ", Method:",
+          methodName,
+          ", Overload Index:",
+          overloadIndex,
+        );
+        console.log("Return value:", retval);
+      }
 
-          checkFieldsRecursive(retval);
+      checkFieldsRecursive(retval);
 
-          return retval;
-        };
+      return retval;
+    };
+}
 function checkFieldsRecursive(obj) {
   if (obj === null || obj === undefined) return;
 
-  if (typeof obj === 'object') {
+  if (typeof obj === "object") {
     for (const key in obj) {
       if (obj.hasOwnProperty(key)) {
         checkFieldsRecursive(obj[key]);
